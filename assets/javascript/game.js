2f01db40dc0fda8482828ef61e0b31c3e7255c3b
claire-labry <clairestjlabry@gmail.com>
2020-02-15 22:26:24 -0600

    finally got all of the functions to working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,43 @@
 //this is the array of the alaphabet for the psychic game
 var alaphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//this is where wins, losses, guesses left, and guesses so far will be stored.
+//this is where wins, losses, guesses left, and guessed letters will be stored.
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessedLetters= [];
 
+
+
+//this generates a random number
 var computerChoice = alaphabet[Math.floor(Math.random() * alaphabet.length)];
     console.log(computerChoice) 
 
-document.onkeypress = function(event) {
+// this is the function when a user presses a key
+document.onkeyup = function(event) {
     var userGuess = event.key;
-
+    guessedLetters.push(userGuess);
+    
     if (userGuess === computerChoice){
         wins++;
-    }else{
-        guesses--;
-    } if (guesses === 0){
-        losses++
+        guessesLeft = 10;
+        guessesLetters = [];
+
+        alert("Why yes, you've won this round. You're destined to be a fortune teller!")
+
+    } else {
+        guessesLeft--;
+
+    } if (guessesLeft === 0){
+        losses++;
+        guessesLeft =10;
+        guessedLetters = [];
+
+        alert ("YOU ARE NOT A SOOTHSAYER! ATTEMPT AGAIN!")
     }
+   
     document.getElementById("wins").innerHTML = "Wins: " + wins;
     document.getElementById("losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guesses-left").innerHTML = "Guesses Left: " + guessesLeft;
-    document.getElementById("guessed-letters").innerHTML = "Guesses Left: " + guessedLetters;
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
 }
