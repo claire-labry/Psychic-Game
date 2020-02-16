866bc62dc33326ff3b4bc0c94ee9201e1cfe360b
claire-labry <clairestjlabry@gmail.com>
2020-02-15 21:01:53 -0600

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,27 @@
-
 //this is the array of the alaphabet for the psychic game
-var psychicChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var alaphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //this is where wins, losses, guesses left, and guesses so far will be stored.
 var wins = 0;
 var losses = 0;
-var guessesLeft = 15;
-var attemptedLetters = [];
+var guessesLeft = 9;
+var guessedLetters= [];
 
+var computerChoice = alaphabet[Math.floor(Math.random() * alaphabet.length)];
+    console.log(computerChoice) 
 
-//the event listner
-document.onekeyup = function(event){
-    var userChoice = event.key;
+document.onkeypress = function(event) {
+    var userGuess = event.key;
 
-//this is the function when the psychic (computer) picks 
-    var computerChoice = psychicChoices[Math.floor(Math.random() * psychicChoices.length)]
-    console.log("Your Psychic desired " + psychicChoices);
-}
-
-document.getElementById("letters").reset();{
-    guessesLeft = 15;
-    attemptedLetters =[];
-}
-
-//this is when the user is correct
-
-if(userChoice !== computerChoices){
-    document.getElementById("guessesLeft").innerText = guessesLeft;
-    document.getElementById("letters").innerText = "ATTEMPT AGAIN!";
-    attemptedLetters.push(userChoice);
-    document.getElementById("attempedletters").innerText = attemptedLetters;
-} else if(userChoice === computerChoice){
-    wins++;
-    alert("YOU CAN SEE THE FUTURE! YOU ARE A PSYCHIC!");
-    document.getElementById("Wins")
-} if (attemptedLetters === 0){
-    losses++;
-    document.getElementById("letters").innerText ="YOU DON'T HAVE A FUTURE! ATTEMPT AGAIN!";
-    resize
+    if (userGuess === computerChoice){
+        wins++;
+    }else{
+        guesses--;
+    } if (guesses === 0){
+        losses++
+    }
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guesses-left").innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById("guessed-letters").innerHTML = "Guesses Left: " + guessedLetters;
 }
